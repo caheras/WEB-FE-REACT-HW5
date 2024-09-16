d8499e0dc6e96d0edcbda4c40ea2505e245d9812
Camilo Heras <caheras@uninorte.edu.co>
2024-09-16 16:56:02 -0500

    Add deployment workflow and configure base URL

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/WEB-FE-REACT-HW5/',
   css: {
     postcss: path.resolve(__dirname, './postcss.config.js'),
   },
